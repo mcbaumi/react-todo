5684edf0d02ff08fa72fff4f2235279513e12370
Damien Baumgartner <baumi.devl@gmail.com>
2017-02-18 18:10:01 +0100

    Add todos reducer and tests

--- app/tests/reducers/reducers.test.jsx
+++ app/tests/reducers/reducers.test.jsx
@@ -26,4 +26,40 @@ describe('Reducers', () => {
       expect(res).toBe(true);
     });
   });
+
+  describe('todosReducer', () => {
+    it('should add new todo', () => {
+      var action = {
+        type: 'ADD_TODO',
+        text: 'Something to do'
+      };
+      var res = reducers.todosReducer(df([]), df(action));
+
+      expect(res.length).toBe(1);
+      expect(res[0].text).toBe(action.text);
+    });
+
+    it('should toggle todo', () => {
+      var todos = [
+        {
+          id: 1,
+          text: 'Running',
+          completed: false
+        },
+        {
+          id: 2,
+          text: 'Walking',
+          completed: false
+        }
+      ];
+      var action = {
+        type: 'TOGGLE_TODO',
+        id: 2
+      };
+      var res = reducers.todosReducer(df(todos), df(action));
+
+      expect(res[1].completed).toBe(true);
+      expect(res[1].completedAt).toExist();
+    });
+  });
 });
